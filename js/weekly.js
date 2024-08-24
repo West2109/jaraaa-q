e627c250bba93b62493d5a4c43ea6c21c00702f0
West2109 <deiionsoflegend@gmail.com>
2024-08-24 19:14:36 +0700

    Update weekly.js

--- js/weekly.js
+++ js/weekly.js
@@ -1,4 +1,4 @@
-const letterContent = "haii jaraa!,\n\njangan lupa minum vitamin yaaa, walaupun suda tida paskib lagi harus tetep minum biar kuat kaya galpin!!! :D";
+const letterContent = "jangan lupa minum vitamin yaaa, walaupun suda tida paskib lagi harus tetep minum biar kuat kaya galpin!!! :D/n06:10 PM/n/nmaaf./n07:14 PM";
 let index = 0;
 
 function typeLetter() {
